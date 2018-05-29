4a1f0073c0cf2854e1054bf529fc4a07fc17f09c
Kai Yao <kai.b.yao@gmail.com>
2018-05-29 00:29:56 -0500

    added editorconfig, prettier, lint-staged, eslint, etc.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,45 +1,56 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 module.exports = {
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-
-      options: {
-        presets: ['env']
-      }
-    }, {
-      test: /\.(scss|css)$/,
-
-      use: [{
-        loader: MiniCssExtractPlugin.loader
-      }, {
-        loader: 'css-loader',
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
 
         options: {
-          sourceMap: true
-        }
-      }, {
-        loader: 'sass-loader',
+          presets: ['env'],
+        },
+      },
+      {
+        test: /\.(scss|css)$/,
 
-        options: {
-          sourceMap: true
-        }
-      }]
-    }]
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
   },
 
   plugins: [
-    new UglifyJSPlugin(),
-    new MiniCssExtractPlugin({ filename:'lwechurch.[chunkhash].css' })
+    new UglifyJsPlugin(),
+    new MiniCssExtractPlugin({ filename: 'lwechurch.[chunkhash].css' }),
   ],
 
-  entry: ./static_src/index,
+  entry: './static_src/index',
 
   output: {
     filename: '[name].[chunkhash].js',
-    path: path.resolve(__dirname, 'static')
+    path: path.resolve(__dirname, 'static'),
   },
 
-  mode: 'production'
-}
+  mode: 'production',
+};
