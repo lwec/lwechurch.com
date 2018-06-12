feaa60e16554732e43cafcfda8d4b96df8f5283b
Kai Yao <kai.b.yao@gmail.com>
2018-06-12 01:20:18 -0500

    less parallax

--- static_src/index.js
+++ static_src/index.js
@@ -5,7 +5,7 @@ require('./styles/lw14-base.less');
 // Parallax effect
 // eslint-disable-next-line no-new
 new Rellax('.lw14-page-intro header', {
-  speed: -4,
+  speed: -2,
 });
 
 // Set up mobile nav toggle
